93ff13205e6a743100ccbf6a58c94f50e839e734
Tom Willis <tw395@cam.ac.uk>
2022-05-29 20:55:01 +0100

    Added noPerturb version of the GPS CLI program

---
+++ apps/computeGpsNoPerturbCLI.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <gps.hpp>
+#include <boost/program_options.hpp>
+#include <rapidcsv.h>
+
+namespace po = boost::program_options;
+using namespace po;
+using namespace gps;
+using namespace rapidcsv;
+
+int main(int argc, const char* argv[]) {
+  po::options_description desc("Allowed options");
+
+  std::string inputFile;
+  std::string outputFile;
+  std::string traitA;
+  std::string traitB;
+  std::string colLabelA;
+  std::string colLabelB;
+
+  desc.add_options()
+    ("help", "Print help message")
+    ("inputFile,i", po::value<std::string>(&inputFile), "Path to input file")
+    ("colLabelA,a", po::value<std::string>(&colLabelA), "Label of column A")
+    ("colLabelB,b", po::value<std::string>(&colLabelB), "Label of column B")
+    ("traitA,c", po::value<std::string>(&traitA), "Trait A")
+    ("traitB,d", po::value<std::string>(&traitB), "Trait B")
+    ("outputFile,o", po::value<std::string>(&outputFile), "Path to output file")
+    ;
+
+  po::variables_map vm;
+  po::store(po::parse_command_line(argc, argv, desc), vm);
+  po::notify(vm);
+
+  if(vm.count("inputFile")) {
+    Document data(inputFile, LabelParams(), SeparatorParams('\t'));
+
+    std::vector<double> u = data.GetColumn<double>(colLabelA);
+    std::vector<double> v = data.GetColumn<double>(colLabelB);
+
+    double gps = gpsStat(u, v);
+
+    std::stringstream stringOutput;
+
+    stringOutput << "Trait_A\tTrait_B\tGPS" << std::endl;
+
+    stringOutput << traitA << "\t" << traitB << "\t" << gps << std::endl;
+
+    Document output(stringOutput, LabelParams(), SeparatorParams('\t'));
+    output.Save(outputFile);
+  } else {
+      std::cout << desc << std::endl;
+  }
+
+  return 0;
+}
