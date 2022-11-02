4c5d1ba2e8f4a1043b24bb237bb65e6d6a5819c5
Tom Willis <tw395@cam.ac.uk>
2022-11-02 12:10:06 +0000

    Refined R script

--- R/fit_gevd_and_compute_pvalue.R
+++ R/fit_gevd_and_compute_pvalue.R
@@ -3,7 +3,7 @@ library(evd)
 library(fitdistrplus)
 
 parser <- ArgumentParser(description = 'Computes p-value for GPS statistic using permuted data')
-parser$add_argument('-g', '--gps', type = 'numeric', help = 'GPS statistic computed using non-permuted data', required = T)
+parser$add_argument('-g', '--gps', type = 'double', help = 'GPS statistic computed using non-permuted data', required = T)
 parser$add_argument('-p', '--perm_file', type = 'character', help = 'Path to file containing realisations of the GPS value generated from permuted data', required = T)
 parser$add_argument('-o', '--output_file', type = 'character', help = 'Path to output file', required = T)
 
@@ -34,4 +34,4 @@ shape.sd <- fgev.fitdist$sd[['shape']]
 
 pvalue <- pgev(args$gps, loc = loc, scale = scale, shape = shape, lower.tail = F)
 
-write.table(data.frame(gps = args$gps, n = nrow(perm_dat), loc = loc, loc.sd = loc.sd, scale = scale, scale.sd = scale.sd, shape = shape, shape.sd = shape.sd, pval = pvalue), sep = '\t', file = args$output_file)
+write.table(data.frame(gps = args$gps, n = length(perms), loc = loc, loc.sd = loc.sd, scale = scale, scale.sd = scale.sd, shape = shape, shape.sd = shape.sd, pval = pvalue), sep = '\t', file = args$output_file, quote = F, row.names = F)
