7710959e3bd8755723d85752df31c5fa5a76716f
Tom Willis <tw395@cam.ac.uk>
2022-11-09 12:15:34 +0000

    PP ecdf algorithm implementation passes simple unit tests

--- PPEcdf/PPEcdf.cpp
+++ PPEcdf/PPEcdf.cpp
@@ -4,6 +4,12 @@
 #include <numeric>
 #include <algorithm>
 #include <cassert>
+#include <iostream>
+#include <boost/multi_index_container.hpp>
+#include <boost/multi_index/ranked_index.hpp>
+#include <boost/multi_index/member.hpp>
+
+using namespace boost::multi_index;
 
 namespace PPEcdf {
 
@@ -12,26 +18,35 @@ namespace PPEcdf {
 
     size_t n = u.size();
 
+    std::map<std::pair<double, double>, size_t> posMap;
+
+    // TODO duplicate keys? Perhaps could handle with vector
+    for(size_t i = 0; i < n; i++) {
+      posMap[{u[i], v[i]}] = i;
+    }
+
     std::vector<size_t> idx = idxSort(u);
 
     std::vector<double> u_sorted = reindex(u, idx);
 
     std::vector<double> v_sorted = reindex(v, idx);
 
-    std::multiset<double> v_set;
-
     std::vector<double> ecdf(n);
 
+    typedef multi_index_container<double, indexed_by<ranked_non_unique<identity<double>>>> double_multiset;
+
+    double_multiset v_set;
+
     for(size_t i = 0; i < n; i++) {
-      auto it = v_set.insert(v_sorted[i]);
+      double_multiset::iterator it = v_set.insert(v_sorted[i]).first;
 
-      size_t m = 0;
+      double_multiset::size_type m = v_set.rank(it);
 
-      while(it != v_set.begin()) {
-
-      }
+      ecdf[posMap[{u_sorted[i], v_sorted[i]}]] = (double) (m+1) / n;
 
     }
+
+    return ecdf;
   }
 
   // From https://stackoverflow.com/a/12399290
@@ -41,8 +56,9 @@ namespace PPEcdf {
 
     std::iota(idx.begin(), idx.end(), 0);
 
+    // TODO Is this still stable?
     std::stable_sort(idx.begin(), idx.end(),
-                [&v](size_t i1, size_t i2) {return v[i1] < v[i2];});
+                [&v](size_t i1, size_t i2) {return v[i1] <= v[i2];});
 
     return idx; 
   }
@@ -53,7 +69,7 @@ namespace PPEcdf {
     std::vector<double> u(v.size());
 
     for(size_t i = 0; i < idx.size(); i++) {
-      u[i] = v[idx[i]];
+      u[idx[i]] = v[i];
     }
 
     return u;
---
+++ test/testPPEcdf.cpp
@@ -0,0 +1,219 @@
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+#include <PPEcdf.hpp>
+#include <iostream>
+#include <rapidcsv.h>
+
+using namespace rapidcsv;
+using namespace PPEcdf;
+using namespace Catch;
+
+TEST_CASE( "idxSort sorts simple data", "[idxSort]" ) {
+  std::vector<double> v({.5, .4, .3, .2, .1});
+  std::vector<size_t> idx({4, 3, 2, 1, 0});
+
+  std::vector<size_t> sortedIdx = idxSort(v);
+
+  REQUIRE_THAT(
+               sortedIdx,
+               Matchers::Equals(std::vector<size_t>{4, 3, 2, 1, 0}));
+}
+
+TEST_CASE( "idxSort sorts simple data with duplicates", "[idxSort]" ) {
+  std::vector<double> v({.4, .4, .3, .2, .1});
+  std::vector<size_t> idx({4, 3, 2, 1, 0});
+
+  std::vector<size_t> sortedIdx = idxSort(v);
+
+  REQUIRE_THAT(
+               sortedIdx,
+               Matchers::Equals(std::vector<size_t>{4, 3, 2, 1, 0}));
+}
+
+TEST_CASE( "idxSort sorts simple data with all duplicates but one", "[idxSort]" ) {
+  std::vector<double> v({.4, .4, .4, .4, .1});
+  std::vector<size_t> idx({4, 3, 2, 1, 0});
+
+  std::vector<size_t> sortedIdx = idxSort(v);
+
+  REQUIRE_THAT(
+               sortedIdx,
+               Matchers::Equals(std::vector<size_t>{4, 3, 2, 1, 0}));
+}
+
+TEST_CASE( "reindex sorts simple data", "[reindex]" ) {
+  std::vector<double> v({.5, .4, .3, .2, .1});
+  std::vector<size_t> idx({1, 2, 4, 3, 0});
+
+  std::vector<double> reindexedV = reindex(v, idx);
+
+  REQUIRE_THAT(
+               reindexedV,
+               Matchers::Approx(std::vector<double>{.1, .5, .4, .2, .3}));
+}
+
+TEST_CASE( "bivariatePPEcdf computes the ecdf for simple data", "[bivariatePPEcdf]" ) {
+  std::vector<double> v({.5, .4, .3, .2, .1});
+  std::vector<double> u({.5, .4, .3, .2, .1});
+
+  std::vector<double> ecdf = bivariatePPEcdf(u, v);
+
+  REQUIRE_THAT(
+               ecdf,
+               Matchers::Approx(std::vector<double>{1, .8, .6, .4, .2}));
+}
+
+TEST_CASE( "bivariatePPEcdf computes the ecdf for less simple data", "[bivariatePPEcdf]" ) {
+  std::vector u({.1, .2, .3, .4, .5});
+  std::vector v({.1, .2, .3, .5, .4});
+
+  std::vector<double> ecdf = bivariatePPEcdf(u, v);
+
+  REQUIRE_THAT(
+               ecdf,
+               Matchers::Approx(std::vector<double>{.2, .4, .6, .8, .8}));
+}
+
+/*
+TEST_CASE( "ecdf estimates ecdf correctly on unsorted simple data set", "[ecdf]" ) {
+  std::vector u({.1, .2, .5, .4, .3});
+
+  std::vector<double> ecdfResult = ecdf(u);
+
+  REQUIRE_THAT(
+               ecdfResult,
+               Matchers::Approx(std::vector<double>{.2, .4, 1., .8, .6})
+               );
+}
+
+TEST_CASE( "gps computes correct statistic on uniformly distributed data", "[gps]" ) {
+
+  Document doc("test/data/1e3_unif.csv");
+
+  std::vector<double> u = doc.GetColumn<double>("u");
+  std::vector<double> v = doc.GetColumn<double>("v");
+
+  double gpsResult = gpsStat(u,v, &bivariateEcdfLW);
+
+  REQUIRE(gpsResult == Detail::Approx(0.87333));
+}
+
+TEST_CASE( "gpsStat runs in simple case", "[gpsStat]" ) {
+  std::vector u({.1, .2, .3, .4, .5});
+  std::vector v({.1, .2, .3, .5, .4});
+
+  double gpsResult = gpsStat(u,v, &bivariateEcdfLW);
+
+  REQUIRE(gpsResult == Detail::Approx(1.439137));
+}
+
+
+TEST_CASE( "Naive bivariate ecdf runs in simple case", "[ecdf]" ) {
+  std::vector u({.1, .2, .3, .4, .5});
+  std::vector v({.1, .2, .3, .5, .4});
+
+  std::vector<double> ecdfResult = bivariateEcdfPar(u,v);
+
+  std::cout << ecdfResult[0] << std::endl;
+
+  REQUIRE_THAT(
+               ecdfResult,
+               Matchers::Approx(std::vector<double>{.2, .4, .6, .8, .8})
+               );
+}
+
+TEST_CASE( "L&W bivariate ecdf runs in simple case", "[ecdf]" ) {
+  std::vector u({.1, .2, .3, .4, .5});
+  std::vector v({.1, .2, .3, .5, .4});
+
+  std::vector<double> ecdfResult = bivariateEcdfLW(u,v);
+
+  REQUIRE_THAT(
+               ecdfResult,
+               Matchers::Approx(std::vector<double>{.2, .4, .6, .8, .8})
+               );
+}
+
+TEST_CASE( "L&W bivariate ecdf throws exception with differently-sized input vectors", "[ecdf]") {
+
+  REQUIRE_THROWS_MATCHES(
+                         bivariateEcdfLW(std::vector<double>{.1, .2}, std::vector<double>{.1, .3, .05}),
+                         std::invalid_argument,
+                         Message("Size of u and v differs.")
+                         );
+}
+
+TEST_CASE( "L&W bivariate ecdf computes correct ecdf for uniformly distributed data", "[ecdf]" ) {
+
+  Document data("test/data/1e3_unif.csv");
+  Document exemplar("test/data/1e3_unif_ecdf.csv");
+
+  std::vector<double> u = data.GetColumn<double>("u");
+  std::vector<double> v = data.GetColumn<double>("v");
+  std::vector<double> ecdfExemplar = exemplar.GetColumn<double>("ecdf");
+
+  std::vector<double> ecdf = bivariateEcdfLW(u,v);
+
+  REQUIRE_THAT(
+               ecdf,
+               Matchers::Approx(ecdfExemplar)
+               );
+
+}
+
+TEST_CASE( "Naive bivariate ecdf computes correct ecdf for uniformly distributed data", "[ecdf]" ) {
+
+  Document data("test/data/1e3_unif.csv");
+  Document exemplar("test/data/1e3_unif_ecdf.csv");
+
+  std::vector<double> u = data.GetColumn<double>("u");
+  std::vector<double> v = data.GetColumn<double>("v");
+  std::vector<double> ecdfExemplar = exemplar.GetColumn<double>("ecdf");
+
+  std::vector<double> ecdf = bivariateEcdfPar(u,v);
+
+  REQUIRE_THAT(
+               ecdf,
+               Matchers::Approx(ecdfExemplar)
+               );
+
+}
+
+
+TEST_CASE( "Naive bivariate ecdf throws exception with differently-sized input vectors", "[ecdf]") {
+  REQUIRE_THROWS_MATCHES(
+                         bivariateEcdfPar(std::vector<double>{.1, .2}, std::vector<double>{.1, .3, .05}),
+                         std::invalid_argument,
+                         Message("Size of u and v differs.")
+                         );
+}
+
+TEST_CASE( "gps throws exception with differently-sized input vectors", "[ecdf]") {
+
+  REQUIRE_THROWS_MATCHES(
+                         gpsStat(std::vector<double>{.1, .2}, std::vector<double>{.1, .3, .05}, &bivariateEcdfLW),
+                         std::invalid_argument,
+                         Message("Size of u and v differs.")
+                         );
+}
+
+TEST_CASE( "gps throws exception on undefined case", "[ecdf]") {
+
+  REQUIRE_THROWS_MATCHES(
+                         gpsStat(std::vector<double>{.1, .2, .3}, std::vector<double>{.1, .2, .3}, &bivariateEcdfLW),
+                         std::invalid_argument,
+                         Message("Indices of largest elements of u and v coincide. GPS statistic is undefined in this case.")
+                 );
+}
+
+TEST_CASE( "perturbDuplicates distinguishes duplicates in simple case", "[perturbDuplicates]") {
+
+  std::vector<double> u({.1,.1,.1,.2,.3,.4,.5});
+
+  std::vector<double> noDup = perturbDuplicates_addEpsilon(u, 1.0);
+
+  REQUIRE(noDup[0] == .1);
+  REQUIRE(noDup[0] != noDup[1]);
+  REQUIRE(noDup[0] != noDup[2]);
+}
+*/
